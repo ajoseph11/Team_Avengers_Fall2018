1b358d1e9ed7706cb5bcebb272dc8f7db4a7ab0d
ajoseph11 <ajoseph@ggc.edu>
2018-10-27 12:42:26 -0400

    Add Room Details

--- src/game_object/Room.java
+++ src/game_object/Room.java
@@ -1,5 +1,219 @@
 package game_object;
 
 public class Room {
+	int playerHP;
+	 String playerName;
+	 String playerWeapon;
+	 int monsterHP;
 
+	 
+
+	 public void a2() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println(
+	    "There is an eerie feeling in the room. The air is muggy. \n With every step you take another stream of sweat comes down your face.\n You initially see a few pig people. There is a dim fire in the corner,\n you can see the shadow of a chest. There are a few pigs standing around a \ntable. There seems to be a carcass on the wall.");
+	  System.out.println("");
+	  System.out.println("What do you want to do?");
+	  System.out.println("");
+	  System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }
+	 public void b1() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println(
+	    "A cool breeze goes down your back. The pig people seem to be \nsleepwalking â€“ they are moving in a daze. Seems as if gliding past them is an option. \nThere appears to be nothing of use in the roomâ€¦ except, there is a back pack \non the floor, a skeleton, and a few barrels of god-knows-what.");
+	    System.out.println("");
+	  System.out.println("What do you want to do?");
+	 // System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	  System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }
+	 public void b2() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("The door falls open, a gust of wind comes in the room, and the candles mounted against the walls flames flicker. Stomachs are growling: the pig people are on the prowl. A smell of desperation is in the air. The closest object seems to be a crate; there could still be something in there. There is a tool bench, as well as a work table. This room is filled with opportunity – or defeat.");
+	    System.out.println("");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	  System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }
+	 public void c1() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("This room appears to be cluttered with crates, barrels and shelves. They seem to be empty at a first glance, but the amount of furniture in this room is discomforting. This sneak attack is not so sneaky.");
+	    System.out.println("");
+	  System.out.println("What do you want to do?");
+	 // System.out.println("1: Go north");
+	 // System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	  System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }
+	  public void c2() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("Every candle is lit. A desk lies ahead with tools for a scriber. There are two bunkbeds as well as a chair. This must have been where someone rested… or continues to rest. The only way to find out is to tread ahead.");
+	    System.out.println("");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	  System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }
+	   public void a3() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("There is a stench of blood that reeks from a mile away. There are blood stained walls, and spotty places on the floor with built up gunk. The gunk appears to have things in it. What can of things? Let’s scavenge and find out. BUT be careful the undead are lurking.");
+	    System.out.println("");
+	  System.out.println("What do you want to do?");
+	  //System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	 // System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }
+	      public void a4() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("A sight for sore eyes, the undead are ravaging a minions’ carcass. What a monster-eat-monster world this is. There must have been a combat between them, there are a couple of weapons on the floor. Disturbing them during the feast may be fatal.");
+	    System.out.println("");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	 // System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }
+	     public void b3() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("This room looks like its smaller in size, but the gunk on the walls is incredibly thick. Oh no the undead are awakening from the walls. Will you have time to explore the room or will you just die. Put up a fight. ");
+	  System.out.println("");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	 //System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }     
+	    public void b4() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println(" How peculiar, a dusty work table with beakers and tubes. Was this some kind of work area for the sorcerer at some point? There are quite a few items to inspect. Recognize this analysis was done in peace and quiet… but when are the undead ever quiet? Tread lightly!");
+	  System.out.println("");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	  System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }
+	    public void b5() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("Ready for combat, the undead are projecting their vile without warning or warrant. There must be a chest in here, the stakes are too high, but when do monsters ever play by the rules. A winner takes risks, you choose your fate. Explore the room, there is a barrel, mirror, and a crate at first glance. ");
+	  System.out.println("");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  //System.out.println("2: Go east");
+	  //System.out.println("3: Go south");
+	  System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }
+	      public void d1() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("Stepping through the door, you’re hit with a humid fungal stench. The room before you is square and flooded with water. Moving forward from the door you walk down submerged steps until you’re waste high in the filthy pool.");
+	  System.out.println("");
+	  System.out.println("What do you want to do?");
+	  //System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	  //System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }
+	      public void d2() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("Mold grows on the walls just above the water that fills the room, which is still like a corpse. There are probably a few hidden beneath these waters. You hope that you do not join them.");
+	  System.out.println("");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	  System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 } 
+	     public void d3() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("The cold in the air has not stopped the growth of fungal life here. You wonder if this flooding was intentional or just the result of neglect. Perhaps you should hurry and just get through this lest you contract some awful disease.");
+	  System.out.println("");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	  System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 } 
+	      public void e2() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("Foulness that one would have to smell to believe enters your nostrils. Resisting the urge to wretch, you look around the dank room to see a chest. Perhaps there is something of use here after all.");
+	  System.out.println("");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  //System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	  System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }      
+	      public void e3() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("Rotting fish carcasses float in the water that fills this room. Maybe you should turn back. At the very least you visit here should be quick, you think to yourself.");
+	  System.out.println("");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  //System.out.println("2: Go east");
+	  //System.out.println("3: Go south");
+	  System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }   
+	      // edit 
+	      // edit 
+	      // edit 
+	      public void c4() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("(If no lantern description) Stepping through the door, you feel a chill. The room is pitch black. Perhaps you may find a light elsewhere.");
+	  System.out.println("");
+	  System.out.println("(If lantern description) A room with no natural light of its own illuminated by your lantern. Yet, even the lanterns warmth cannot take the edge off the chill you feel.");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  System.out.println("3: Go south");
+	  System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }  
+	      // edit 
+	      // edit 
+	      // edit 
+	     public void c5() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("(If no lantern description) Stepping through the door, you feel a chill. The room is pitch black. Perhaps you may find a light elsewhere.");
+	  System.out.println("");
+	  System.out.println("(If lantern description)  These dark rooms are not so scary with a lantern in hand. At least, as far as you can tell. If only your lantern was as warm as it was bright.");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  //System.out.println("3: Go south");
+	  //System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }  
+	        public void c6() {
+	  System.out.println("\n------------------------------------------------------------------\n");
+	  System.out.println("(If no lantern description) Stepping through the door, you feel a chill. The room is pitch black. Perhaps you may find a light elsewhere.");
+	  System.out.println("");
+	  System.out.println("(If lantern description)  These dark rooms are not so scary with a lantern in hand. At least, as far as you can tell. If only your lantern was as warm as it was bright.");
+	  System.out.println("What do you want to do?");
+	  System.out.println("1: Go north");
+	  System.out.println("2: Go east");
+	  //System.out.println("3: Go south");
+	  //System.out.println("4: Go west");
+	  System.out.println("\n------------------------------------------------------------------\n");
+	 }  
 }
