d6d0e7572dfcb693641097b471e89f6ef202c4d1
ajoseph11 <ajoseph@ggc.edu>
2018-11-01 22:26:48 -0400

    Did a new folder game_object2 that allows to move room to room

--- src/game_object/Main.java
+++ src/game_object/Main.java
@@ -10,8 +10,28 @@ public class Main {
         try{
             Scanner input = new Scanner (file) ;
             while (input.hasNext()){
+                // read each line from the text file
                 String nums = input.nextLine();
+
+                // process each line read
+                // file has this format:
+                // ID:  ##
+                // Desc: $$$$
+                // Exits: $-$# $-$# $-$# ...
+                // endRoom;
+
                 System.out.println(nums);
+
+                // nums = "ID A2"
+                // use the split method from Scanner to parse in the String read
+
+                String[] words = nums.split(" ");
+
+                for (String word : words)
+                {
+                    // print word to Console for debugging purpose
+                    System.out.println(word);
+                }
             }
         } catch (FileNotFoundException e) {
             System.err.format("File does not exist\n");
