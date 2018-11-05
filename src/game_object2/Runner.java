16dadb9bf1358eabc131964a1f7847496879f9d1
Adefemi Kolawole <akolawole@ggc.edu>
2018-11-04 23:48:03 -0500

    all rooms completely done and tested

--- src/game_object2/Runner.java
+++ src/game_object2/Runner.java
@@ -26,9 +26,9 @@ public class Runner {
 		System.out.println(errorMessage);
 		
 	}
-	//System.out.println("From: " + PuzzleController.class.getName());
-	//System.out.println(PuzzleController.getPuzzleR8().getPuzzleMap().get("Riddle"));
-	//System.out.println(PuzzleController.getPuzzleR1().getPuzzleMap());
+	System.out.println("From: " + PuzzleController.class.getName());
+	System.out.println(PuzzleController.getPuzzleR8().getPuzzleMap().get("Riddle"));
+	System.out.println(PuzzleController.getPuzzleR1().getPuzzleMap());
     
 	
 
@@ -56,8 +56,37 @@ public class Runner {
 	System.out.println(RoomController.roomA3);
 	System.out.println(RoomController.roomA4);
 	System.out.println(RoomController.roomB3);
-	System.out.println(RoomController.roomB4);
-	System.out.println(RoomController.roomB5); // First 10 Rooms being printed in console for testing purposes. 
+	System.out.println(RoomController.roomB4); 
+	System.out.println(RoomController.roomB5); // first 10 rooms
+	
+	
+	System.out.println(RoomController.roomD2);
+	System.out.println(RoomController.roomD1);
+	System.out.println(RoomController.roomD3);
+	System.out.println(RoomController.roomE2);
+	System.out.println(RoomController.roomE3);
+	System.out.println(RoomController.roomC4);
+	System.out.println(RoomController.roomA4);
+	System.out.println(RoomController.roomC5);
+	System.out.println(RoomController.roomD4);
+	System.out.println(RoomController.roomD5);
+	System.out.println(RoomController.roomE4); // second 10 rooms
+	
+	System.out.println(RoomController.roomA1); 
+	System.out.println(RoomController.roomE1); 
+	System.out.println(RoomController.roomA5); 
+	System.out.println(RoomController.roomE5); //4
+	
+	
+	System.out.println(RoomController.entranceChamberRoom); 
+	System.out.println(RoomController.connectingChamberRoom); 
+	System.out.println(RoomController.spiralStaircaseRoom); 
+	System.out.println(RoomController.guardLockedDoorRoom); 
+	System.out.println(RoomController.sorcererBossFightRoom); 
+	System.out.println(RoomController.stairsB1FRoom); 
+	System.out.println(RoomController.necromancerRoom); //7 special
+	
+	System.out.println(RoomController.necromancerRoom); // First 10 Rooms being printed in console for testing purposes. 
  //System.out.println(RoomController.toString());//testing the get method from the Room class to see if an object is actually created. You can aslo change the filed to whicheevr you want. 
 
 		
