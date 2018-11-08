bd523be30dd841ad453a5dc2107093939b409e0f
Adefemi Kolawole <akolawole@ggc.edu>
2018-11-08 11:21:06 -0500

    Attempting to Create a gameController class  as  a replacement for the Runner.java class. This new class  will handle all the sub controller class for the other gaem objects

---
+++ src/controller/GameController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.io.IOException;
+
+import model.Monster;
+/*This used to be the Ruuner class*/
+public class GameController {
+
+	public static void main(String[] args) throws Exception { //throw Exception incase something goes wrong while reading from file.
+		// TODO Auto-generated method stub
+		ItemController.setItemFromText();//This gets item from text, store in map and relates to item object
+		RoomController.setRoomFromText();//This gets rooms from text, store in map and relates to rooms object
+		PuzzleController.setPuzzleFromText();//This gets puzzles  from text, store in map and relates to puzzle object
+
+		
+		// System.out.println(ItemController.getItemW1().getItemID());
+   //System.out.println(ItemController.getItemW1().getItemMap());
+   //System.out.println(ItemController.getItemW2().getItemMap());
+   //PuzzleController.setPuzzleFromText();
+   //System.out.println(ItemController.itemW2);
+   System.out.println(ItemController.getItemk11());
+   
+
+	}
+
+}
