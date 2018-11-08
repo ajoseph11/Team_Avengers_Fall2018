a3cb7c08f40184b244d524433ba4fa56d7711066
Adefemi Kolawole <akolawole@ggc.edu>
2018-11-07 20:52:54 -0500

    edited the ItemController class

--- src/controller/ItemController.java
+++ src/controller/ItemController.java
@@ -13,14 +13,37 @@ public class ItemController {
     //maps for items
     public static Map<String, String> w1Map = new LinkedHashMap<>();
     public static Map<String, String> w2Map = new LinkedHashMap<>();
+    public static Map<String, String> w3Map = new LinkedHashMap<>();
+    public static Map<String, String> w4Map = new LinkedHashMap<>();
+    public static Map<String, String> w5Map = new LinkedHashMap<>();
+    public static Map<String, String> w6Map = new LinkedHashMap<>();
+    public static Map<String, String> w7Map = new LinkedHashMap<>();//w
+    public static Map<String, String> a1Map = new LinkedHashMap<>();
+    public static Map<String, String> a2Map = new LinkedHashMap<>();
+    public static Map<String, String> a3Map = new LinkedHashMap<>();
+    public static Map<String, String> a4Map = new LinkedHashMap<>();
+    public static Map<String, String> a5Map = new LinkedHashMap<>();//a
+    public static Map<String, String> d1Map = new LinkedHashMap<>();
+    public static Map<String, String> d2Map = new LinkedHashMap<>();
+    public static Map<String, String> d3Map = new LinkedHashMap<>();
+    public static Map<String, String> d4Map = new LinkedHashMap<>();
+    public static Map<String, String> d5Map = new LinkedHashMap<>();
+    public static Map<String, String> d6Map = new LinkedHashMap<>();
+    public static Map<String, String> d7Map = new LinkedHashMap<>();//d
+    public static Map<String, String> k11Map = new LinkedHashMap<>();
+    public static Map<String, String> k12Map = new LinkedHashMap<>();
+    public static Map<String, String> k13Map = new LinkedHashMap<>();
+    public static Map<String, String> k15Map = new LinkedHashMap<>();//k
+    public static Map<String, String> c1Map = new LinkedHashMap<>();//c
 
     // error message
     public static String errorMessage;
 
     // items objects
 
-    public static Item itemW1;
-    public static Item itemW2;
+    public static Item itemW1, itemW2,itemW3,itemW4, itemW5,itemW6,itemW7,
+    itema1,itema2,itema3,itema4,itema5,itemd1,itemd2,itemd3,itemd4,itemd5,itemd6,itemd7,
+    itemk11, itemk12, itemk13, itemk15, itemc1;
     
  
 //the method that passes text to item
@@ -39,6 +62,28 @@ public class ItemController {
 		// concrete instantiation of items to store information for each puzzle
         itemW1 = new Item();
         itemW2 = new Item();
+        itemW3 = new Item();
+        itemW4 = new Item();
+        itemW5 = new Item();
+        itemW6 = new Item();
+        itemW7 = new Item();
+        itema1 = new Item();
+        itema2 = new Item();
+        itema3 = new Item();
+        itema4 = new Item();
+        itema5 = new Item();
+        itemd1 = new Item();
+        itemd2 = new Item();
+        itemd3 = new Item();
+        itemd4 = new Item();
+        itemd5 = new Item();
+        itemd6 = new Item();
+        itemd7 = new Item();
+        itemk11 = new Item();
+        itemk12 = new Item();
+        itemk13 = new Item();
+        itemk15 = new Item();
+        itemc1 = new Item();
         
 
 		// concrete instantiation of maps to store information for each puzzle
@@ -65,6 +110,58 @@ public class ItemController {
 
                     itemW1.setItemMap(w1Map);
                     itemW2.setItemMap(w2Map);
+                    itemW3.setItemMap(w3Map);
+                    itemW4.setItemMap(w4Map);
+                    itemW5.setItemMap(w5Map);
+                    itemW6.setItemMap(w6Map);
+                    itemW7.setItemMap(w7Map);
+                    itema1.setItemMap(a1Map);
+                    itema2.setItemMap(a2Map);
+                    itema3.setItemMap(a3Map);
+                    itema4.setItemMap(a4Map);
+                    itema5.setItemMap(a5Map);
+                    itemd1.setItemMap(d1Map);
+                    itemd2.setItemMap(d2Map);
+                    itemd3.setItemMap(d3Map);
+                    itemd4.setItemMap(d4Map);
+                    itemd5.setItemMap(d5Map);
+                    itemd6.setItemMap(d6Map);
+                    itemd7.setItemMap(d7Map);
+                    itemk11.setItemMap(k11Map);
+                    itemk12.setItemMap(k12Map);
+                    itemk13.setItemMap(k13Map);
+                    itemk15.setItemMap(k15Map);
+                    itemc1.setItemMap(c1Map);
+                    
+
+               /*     w1Map,
+                    w2Map,
+                    w3Map,
+                    w4Map,
+                    w5Map,
+                    w6Map,
+                    w7Map,
+                    a1Map,
+                    a2Map,
+                    a3Map,
+                    a4Map,
+                    a5Map,
+                    d1Map,
+                    d2Map,
+                    d3Map,
+                    d4Map,
+                    d5Map,
+                    d6Map,
+                    d7Map,
+                    k11Map,
+                    k12Map,
+                    k13Map,
+                    k15Map,
+                    c1Map*/
+
+
+
+                    
 
                 }
                 else{
@@ -80,13 +177,19 @@ public class ItemController {
 
             }
         }
-            Item[] itemObjects = {itemW1, itemW2};
+            Item[] itemObjects = {itemW1, itemW2,itemW3,itemW4, itemW5,itemW6,itemW7,
+            	    itema1,itema2,itema3,itema4,itema5,itemd1,itemd2,itemd3,itemd4,itemd5,itemd6,itemd7,
+            	    itemk11, itemk12, itemk13, itemk15, itemc1};
 
-            Map [] itemLinkedHashMapObjects = {
-                    itemW1.getItemMap(), itemW2.getItemMap()};
+            
+			Map []  itemLinkedHashMapObjects = {
+                    itemW1.getItemMap(), itemW2.getItemMap(), itemW3.getItemMap(), itemW4.getItemMap(), itemW5.getItemMap() , itemW6.getItemMap()  , itemW7.getItemMap(),
+                    itema1.getItemMap(),itema2.getItemMap(),itema3.getItemMap(),itema4.getItemMap(),itema5.getItemMap(),itemd1.getItemMap(),itemd2.getItemMap(),itemd3.getItemMap(),itemd4.getItemMap(),itemd5.getItemMap(),itemd6.getItemMap(),itemd7.getItemMap(),
+                    itemk11.getItemMap(), itemk12.getItemMap(), itemk13.getItemMap(), itemk15.getItemMap(), itemc1.getItemMap()        
+            };
             
         	// print key and value side by side as seperate entities
-    		for (String keyItem : itemW1.getItemMap().keySet()) {
+    	for (String keyItem : itemW1.getItemMap().keySet()) {
 
     			String key = keyItem.toString();
     			String value = itemW1.getItemMap().get(keyItem).toString();
